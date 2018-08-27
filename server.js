223c22c8058cf530cddb754788147cb486af88d4
Edgar Barajas <edgarsbarajas@yahoo.com>
2018-08-26 21:08:59 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+  res.render('maintenance.hbs');
+});
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (error) => {
+    if(error){
+      console.log(error);
+    }
+  });
+  next();
+});
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+app.get('/', (request, response) => {
+  response.render('home.hbs', {
+    pageTitle: 'Edgar is the best',
+    welcomeMessage: 'Welcome to my website!'
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'Edgar is the best'
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    error: 'Error handling request'
+  });
+});
+
+app.listen(3000, () => {
+  console.log('server is up on port 3000.');
+});
